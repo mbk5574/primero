c6d651613792792869a0a88679e2627fc26f64cc
David <davidmpe2002@gmail.com>
2022-11-27 19:41:40 +0100

    Trabajo 27/11/2022

--- task3.py
+++ task3.py
@@ -17,6 +17,7 @@ class nodo_arbol:
     def __init__(self, nodo, padre):
         global total_nodos
         global estrategia
+        global g
         total_nodos = total_nodos + 1
         self.id = (total_nodos) 
         self.estado = ""
@@ -26,12 +27,14 @@ class nodo_arbol:
         self.accion = ""
         self.nodo_grafo = nodo
         self.costo = 0
+        self.coste = 0
 
         if(padre != ""):
             self.padre = padre
             self.profundidad = self.padre.profundidad + 1
-            self.costo = self.padre.costo+self.costo
-
+            self.coste = g.get_arista(nodo.id, padre.nodo_arbol.id).length
+            self.costo = self.padre.costo + self.coste
+           
         if estrategia == "p":
             self.valor = 1/(self.profundidad + 1)
         elif estrategia == "a":
@@ -50,53 +53,39 @@ class nodo_arbol:
 
 class estado:
 
-    def __init__(self, g, lista_objetivos, estado_anterior, current_nodo, nodo_objetivo):
-        self.g = g
+    def __init__(self, lista_objetivos, current_nodo): #la lista de objetivos, el estado anterior, el nodo
         self.lista_objetivos = lista_objetivos
-        self.nodo_objetivo= nodo_objetivo
-        self.current_nodo = current_nodo
-        self.cadena= ""
-        if nodo_objetivo != "":
-            arista = self.g.get_arista(self.current_nodo.nodo_grafo.id, self.nodo_objetivo.nodo_grafo.id)
-            self.coste = arista.length
-            cadena = "(" + str(self.current_nodo.id) + "->" +str(self.nodo_objetivo.nodo_grafo.id)+",("+ str(self.nodo_objetivo.nodo_grafo.id) + str(self.lista_objetivos) + "),"+str(self.nodo_objetivo.costo)+")"
-            self.cadena=cadena.replace(" ","")
-            self.id = hashlib.md5(cadena.encode("utf-8")).hexdigest()
-            self.estado_anterior = estado_anterior
-        else:
-            self.coste = 0
-           
-    def construir_camino(self):
-        cadena = ""
-        e = self
-        while(e.estado_anterior != ""):
-            cadena = "->" + e.estado_anterior.current_nodo.id
-            e = e.estado_anterior
-        #cadena = cadena[2, len(cadena)]
-        print(cadena)
+        self.current_nodo = current_nodo  # current_nodo y nodo_objetivo son el id del nodo
+        self.cadena = "(" + self.current_nodo.nodo_grafo.id + ",("+ str(self.lista_objetivos) + "))"
+        self.cadena=self.cadena.replace(" ","")
+        self.id = hashlib.md5(self.cadena.encode("utf-8")).hexdigest() #Montamos cadena encriptada en md5, que será el id del estado
 
-def sucesor(nodo = nodo_arbol):
-    if len(nodo.estado.lista_objetivos) == 0: #Si no quedan nodos objetivos, se hace return
-        return
-
-    e = nodo.estado
-    nodos = []
-    adyacentes= SortedList()
-    adyacentes = e.g.adyacencia.get(str(nodo.nodo_grafo.id)) #Recogemos los nodos adyacentes del nodo actual
-    for adyacente in adyacentes: # Por cada nodo adyacente
-        a = nodo.estado.g.lista_nodos.get(adyacente)
-        n = nodo_arbol(a, nodo)
-        e2 = estado(e.g, e.lista_objetivos, e, nodo, n) # Creamos el siguiente estado, siendo el siguiente nodo, el nodo adyacente
-        if adyacente in e2.lista_objetivos: # Si el adyacente esta en la lista de objetivos, le eliminamos de dicha lista
-            e2.lista_objetivos.remove(adyacente)   
+    def toString(self):
+        return self.cadena
         
-        cadena = "(" + str(e.current_nodo) + "->" + str(adyacente) + ",(" + str(adyacente) + "," + str(e2.lista_objetivos) + "," + str(e2.coste) + ")"
+def sucesor(nodo):
+    global g
+    global lista_objetivos
+    nodos = []
+    e = estado(lista_objetivos, nodo)
+    if len(e.lista_objetivos) == 0: #Si no quedan nodos objetivos, se hace return
+        return
+    print(e.toString())
+    adyacentes= SortedList()
+    adyacentes = g.adyacencia.get(str(nodo.nodo_grafo.id)) #Recogemos los nodos adyacentes del nodo actual
+    for adyacente in adyacentes: # Por cada nodo adyacente
+        n = g.lista_nodos.get(adyacente)
+        n_arbol = nodo_arbol(n, nodo)
+        e2 = estado(e.lista_objetivos, n_arbol) # Creamos el siguiente estado, siendo el siguiente nodo, el nodo adyacente
+        if adyacente in lista_objetivos: # Si el adyacente esta en la lista de objetivos, le eliminamos de dicha lista
+            e2.lista_objetivos.remove(adyacente)   
+        cadena = "(" + str(e.current_nodo) + "->" + str(adyacente) + ",(" + str(adyacente) + "," + str(e2.lista_objetivos) + "," + str(n_arbol.coste) + ")"
         cadena=cadena.replace(" ","")
-        n.estado = e2
-        nodos.append(n)
+        print(cadena)
+        n_arbol.estado = e2
+        nodos.append(n_arbol)
         #Siguiente adyacente
     return nodos
-
 
 def algoritmoBusqueda():
     global solucion
@@ -104,16 +93,20 @@ def algoritmoBusqueda():
     global estados_visitados
     global maxdepth
     global estrategia
+    nodo = ""
+
     if (len(frontera) == 0) or solucion:
         nodo.estado.construir_camino()
         return
+
     if estrategia == "p":
         nodo = frontera.pop()
     if estrategia == "a":
-        nodo = frontera.remove(0)
+        nodo = frontera[0]
+        frontera.remove(0)
     if estrategia == "c":
-        #nodo = frontera.elquemenorcostetenga
-        pass
+        nodo = menor_coste()
+        
     print(nodo.estado.cadena)
     if nodo.nodo_grafo.id in nodo.estado.lista_objetivos:
         nodo.estado.lista_objetivos.remove(nodo.nodo_grafo.id)
@@ -131,30 +124,26 @@ def expandir(nodo):
     for i in range(len(nodos)):
         frontera.append[nodos[i]]
 
-def ordenar(nodo):
+def menor_coste():
     global frontera
+    coste = 0
     for i in range(len(frontera)):
-        try:
-            if (nodo.valor < frontera[i].valor) & (nodo.valor > frontera[i+1].valor):
-                frontera.insert(i+1, nodo)
-        except IndexError:
-            frontera.append(nodo)
+        n_coste = frontera[0].costo
+        if n_coste < coste[0]:
+            coste = (n_coste, i)
+    
+    n = frontera[coste[1]]
+    frontera.remove(coste[1])
+    return n
 
 lista = ['1']
 n = "0"
 nodo = g.lista_nodos.get(n)
-e = estado (g, lista, "", nodo, "")
-e.coste = 0
+
 nodo_arb = nodo_arbol(nodo, "")
-
-adyacentes = g.adyacencia.get(str(n))
-for ad in adyacentes:
-    add = g.lista_nodos.get(ad)
-    adyacente = nodo_arbol(add, nodo_arb)
-    e1 = estado(g, lista, e, nodo_arb, adyacente)
-    adyacente.estado = e
-    frontera.add(adyacente)
-
+e = estado (lista, nodo_arb)
+nodo_arb.estado = e
+frontera.append(nodo_arb)
 maxdepth = 1000
 
 algoritmoBusqueda()
